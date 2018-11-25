2be79964b388bdd05539659a033a81e360e9d1be
KillZoom <danghuyvms@gmail.com>
2018-11-25 18:41:57 +0700

    updates lan 2!

--- src/tongquanve_kdl_bien/lesson3_day1/bai2_vehicle/main.java
+++ src/tongquanve_kdl_bien/lesson3_day1/bai2_vehicle/main.java
@@ -18,35 +18,33 @@ public class main {
             System.out.println("3.Thoat");
             System.out.println("Nhap lua chon cua ban : ");
             int luaChon = sc1.nextInt();
-            int dem =0;
-            switch(luaChon){
+            int dem = 0;
+            switch (luaChon) {
 
-                case 1 : {
-                    System.out.println("Nhap so luong xe : ");
-                    int soXe = sc1.nextInt();
-                    for (int i = dem; i < soXe; i++) {
-                        vehicles = new Vehicle();
-                        vehicles.nhapThongTin(sc1);
-                        vehicleArrayList.add(vehicles);
-                        dem++;
-                    }
-
+                case 1: {
+                    Vehicle vehicle1 = new Vehicle("Huy","Honda", 120, 12000000);
+                    Vehicle vehicle2 = new Vehicle("Cuong", "Kaiki", 230, 21000000);
+                    Vehicle vehicle3 = new Vehicle("Quyet", "Yamaha", 345, 40000000);
+                    vehicleArrayList.add(vehicle1);
+                    vehicleArrayList.add(vehicle2);
+                    vehicleArrayList.add(vehicle3);
+                    System.out.println("Thong tin da duoc nhap vao!");
                     break;
                 }
-                case 2 : {
+                case 2: {
                     System.out.println("Thong tin xe : ");
                     System.out.println("Ten Chu Xe\t\t\t" + "Loai Xe\t\t\t" + "Dung Tich\t\t\t" + "Tri Gia\t\t\t"
                             + "Thue Phai Nop\t\t\t");
                     System.out.println("======================================================================================");
-                    for(int i=0; i<vehicleArrayList.size();i++){
+                    for (int i = 0; i < vehicleArrayList.size(); i++) {
                         vehicleArrayList.get(i).xuatThongTin();
                     }
                     break;
                 }
-                case 3 :
+                case 3:
                     kiemTra = false;
             }
-        }while(kiemTra);
+        } while (kiemTra);
 
     }
 }
